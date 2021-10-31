1663051d166f9672875ff94235666d320b951391
NikitaKirpichev <62098494+NikitaKirpichev@users.noreply.github.com>
2021-10-31 22:30:11 +0300

    add some fk

--- src/sql/create_db.sql
+++ src/sql/create_db.sql
@@ -91,3 +91,36 @@ CREATE TABLE `orders`(
 );
 
 
+ALTER TABLE user_info
+	ADD CONSTRAINT FK_user_info_user_id_users_id
+	FOREIGN KEY(user_id) REFERENCES users(id)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE;
+        
+ALTER TABLE users_roles
+	ADD CONSTRAINT FK_user_roles_user_id_users_id
+	FOREIGN KEY(user_id) REFERENCES users(id)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE;
+        
+ALTER TABLE users_roles
+	ADD CONSTRAINT FK_users_roles_role_id_roles_id
+	FOREIGN KEY(role_id) REFERENCES roles(id)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE;
+        
+ALTER TABLE cart
+	ADD CONSTRAINT FK_cart_user_id_users_id
+	FOREIGN KEY(user_id) REFERENCES users(id)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE;
+
+ALTER TABLE cart
+	ADD CONSTRAINT FK_cart_order_id_orders_id
+	FOREIGN KEY(order_id) REFERENCES orders(id)
+		ON DELETE CASCADE 
+		ON UPDATE CASCADE;
+
+ALTER TABLE orders
+
+ALTER TABLE orders
