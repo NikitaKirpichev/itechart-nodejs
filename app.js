e639e6293f983d6b386b429d20c54c130bc3a4f7
NikitaKirpichev <62098494+NikitaKirpichev@users.noreply.github.com>
2021-11-09 18:13:30 +0300

    finish app.js

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 
-app.listen(port, () => {
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const router = express.Router();
+app.use(router);
+
+router.use((err,req,res,next) => {
+    if (err){
+        return res.send(err.message);
+    }
+})
+
+app.listen(PORT, err => {
+    if (err){
+        return console.log(`Cannot listen on port ${port}`);
+    }
     console.log(`App listen on port ${port}`);
 })
